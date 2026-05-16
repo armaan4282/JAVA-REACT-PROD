fa41bbdeb4369fdc2bd1627e3b1cd80dcdb5475c
armaan4282 <shaikhatesham786@gmail.com>
2026-05-16 06:23:32 +0530

    Initial 2 tier application

---
+++ backend/src/main/java/com/example/app/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.example.app.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.app.entity.User;
+import com.example.app.repository.UserRepository;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class UserController {
+
+    @Autowired
+    private UserRepository repository;
+
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return repository.save(user);
+    }
+
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+
+        Optional<User> dbUser =
+                repository.findByEmail(user.getEmail());
+
+        if (dbUser.isPresent()
+                &&
+                dbUser.get().getPassword()
+                        .equals(user.getPassword())) {
+
+            return "Login Success";
+        }
+
+        return "Invalid Credentials";
+    }
+}
