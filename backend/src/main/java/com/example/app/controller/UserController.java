65075f46020a5dfe40c900e8073da3576ecf32c8
armaan4282 <shaikhatesham786@gmail.com>
2026-05-16 07:03:13 +0530

    Update UserController.java

--- backend/src/main/java/com/example/app/controller/UserController.java
+++ backend/src/main/java/com/example/app/controller/UserController.java
@@ -14,7 +14,7 @@ import com.example.app.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     @Autowired
